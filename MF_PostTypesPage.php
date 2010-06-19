6ca224267050ac5d45ca00ef7c92653e3a86c951
David Valdez <me@gnuget.org>
2010-06-19 15:52:59 -0500

    adding the markup for add custom types

--- MF_PostTypesPage.php
+++ MF_PostTypesPage.php
@@ -9,18 +9,41 @@ Class MF_PostTypePages{
 	 *
 	 * @todo Add a more descriptive help
 	 */
-	function top_menu(){
+	function TopMenu(){
 		global $mf_domain;
 		// Add top menu
 		/**
 		 * For now this menu is only displayed for the admin user, i think to this will change soon 
 		 */ 
-		$post_type_screen = add_menu_page(__('Magic Fields Post Types',$mf_domain), __('Post Types',$mf_domain),10,'mf_posttypes',array('MF_PostTypePages','ManagePosttype'));
-		add_submenu_page('mf_posttypes',NULL,NULL,10,"mf_posttype_add",array('MF_PostTypePages','AddPosttype')); 
+		$post_type_screen = add_menu_page(__('Magic Fields Post Types',$mf_domain), __('Post Types',$mf_domain),10,'mf_posttypes',array('MF_PostTypePages','Dispacher'));
 	
+
 		add_contextual_help($post_type_screen,
-			'<p>' . __('Hi you can create and manage custom pages here'). '</p>'
+			'<p>' . __('Hi you can create and manage Post Types here'). '</p>'
 		);
+	}
+
+	/** 
+	 *
+	 *
+	 */
+	function Dispacher(){
+		if(empty($_GET['action'])){
+			$action = "manage";
+		}else{
+			$action = $_GET['action'];
+		}
+		
+		$action = esc_attr($action);
+		
+		switch($action){
+			case "manage":
+				MF_PostTypePages::ManagePostType();
+			break;	
+			case "add":
+				MF_PostTypePages::AddPostType();
+			break;	
+		}
 	}
 
 	/**
@@ -29,9 +52,7 @@ Class MF_PostTypePages{
 	 *
 	 */
 	function ManagePosttype(){
-		global $mf_domain,$_wp_contextual_help;
-		$title = __('Manage Custom Types');
-
+		global $mf_domain;
 
 		print 
 			"<div class ='wrap'>".
@@ -43,6 +64,7 @@ Class MF_PostTypePages{
 							"<th>".__('ID',$mf_domain)."</th>".
 							"<th>".__('Post Type Name',$mf_domain)."</th>".
 							"<th>".__("Capability Type",$mf_domain)."</th>".
+							"<th>".__("Actions",$mf_domain)."</th>".
 						"</tr>".	
 					"</thead>".
 					"<tbody>".
@@ -50,28 +72,87 @@ Class MF_PostTypePages{
 							"<td>1</td>".
 							"<td>Custom Type Demo 1</td>".
 							"<td>Post</td>".
+							"<td>".__('Edit')."|".__('Delete')."</td>".
 						"</tr>".
 					"</tbody>".
 					"<tfoot>".
 						"<tr>".
 							"<th>".__('ID',$mf_domain)."</th>".
 							"<th>".__('Post Type Name',$mf_domain)."</th>".
-							"<th>".__("Capability Type",$mf_domain)."</th>".
+							"<th>".__('Capability Type',$mf_domain)."</th>".
+							"<th>".__('Actions',__($mf_domain))."</th>".
 						"</tr>".	
 					"</tfoot>".
 				"</table>".
 				"<p class='submit'>".
-					"<a href='#' class='button tagadd'>".__('Add a new Custom Type',$mf_domain)."</a>".
+					"<a href='admin.php?page=mf_posttypes&action=add' class='button tagadd'>".__('Add a new Custom Type',$mf_domain)."</a>".
 				"</p>".
 			"</div>";
 	}
 
 	/**
-	 *
-	 *
+	 * Display the form for add a new type of custom type
+	 * 
+	 * @todo Add a section for the capabilities
+	 * @todo Add section of REWRITE
+	 * @todo Improve the help text
 	 */
 	function AddPosttype(){
-		echo "Hola";
+		global $mf_domain;
+
+		print
+			"<div class='wrap'>".
+				"<div id='icon-edit-pages' class='icon32'></div>".
+				"<h2>".__('Add New Post Type',$mf_domain)."</h2>".
+				"<form method='POST' action=''>".
+					"<div class='mf_form'>".
+						"<div class='form-field'>".
+							"<label for='post_type_name'>".__('Name',$mf_domain)."<span>*</span>:</label>".
+							"<input type='text' id='post_type_name' maxlength='20' size='23' name='post_type_name' value=''/>".
+							"<p>".__('Put the name of your Post Type (the max limit is 20 characters')."</p>".
+						"</div>".	
+						"<div class='form-field'>".
+							"<label for='description'>".__('Description',$mf_domain).":<span>*</span></label>".
+							"<textarea name='description'></textarea>".
+							"<p>".__('description about for what is your custom post type',$mf_domain)."</p>".
+						"</div>".	
+						"<div class='form-field'>".
+							"<input type='checkbox' name='is_public' /><label for='is_public' class='label_checkbox'>".__('Is Public',$mf_domain)."</label>".
+							"<p>".__('This post type is public?',$mf_domain)."</p>".
+							"<div class='sub-options is_public_options'>".
+								"<input type='checkbox' name='display_ui' /><label for='display_ui' class='label_checkbox'>".__('Show UI',$mf_domain)."</label>".
+								"<p>".__('This type will be displayed into the Administration menu?',$mf_domain)."</p>".
+								"<input type='checkbox' name='exclude_search' /><label for='exclude_search' class='label_checkbox'>".__('Exclude Search',$mf_domain)."</label>".
+								"<p>".__('This element will be exclude in the search page',$mf_domain)."</p>".
+							"</div>".
+						"</div>".
+						"<div class='form-field'>".
+							"<input type='checkbox' name='supports' /><label class='label_checkbox' for='supports'>".__('Customize which Fields will be displayed in this post type',$mf_domain)."</label>".
+							"<p></p>".
+							"<div class='sub-options supports_options'>".
+								"<input type='checkbox' name='title'><label class='label_checkbox' for='title'>".__('Titlte',$mf_domain)."</label>".
+								"<p>".__('Title',$mf_domain)."</p>".
+								"<input type='checkbox' name='editor'><label class='label_checkbox' for='editor'>".__('Editor',$mf_domain)."</label>".
+								"<p>".__('The user put here the content of the post',$mf_domain)."</p>".
+								"<input type='checkbox' name='comments'><label class='label_checkbox' for='comments'>".__('Comments',$mf_domain)."</label>".
+								"<p>".__('This post type have comments',$mf_domain)."</p>".
+								"<input type='checkbox' name='trackbacks'><label class='label_checkbox' for='trackbacks'>".__('Trackbacks',$mf_domain)."</label>".
+								"<p>".__('This post type have trackbacks',$mf_domain)."</p>".
+								"<input type='checkbox' name='revisions'><label class='label_checkbox' for='revisions'>".__('Revisions',$mf_domain)."</label>".
+								"<p>".__('This post type have revisions',$mf_domain)."</p>".
+								"<input type='checkbox' name='author'><label class='label_checkbox' for='author'>".__('Author',$mf_domain)."</label>".
+								"<p>".__('Author',$mf_domain)."</p>".
+								"<input type='checkbox' name='excerpt'><label class='label_checkbox' for='excerpt'>".__('Excerpt',$mf_domain)."</label>".
+								"<p>".__('This post type have Excerpt',$mf_domain)."</p>".
+								"<input type='checkbox' name='thumbnail'><label class='label_checkbox' for='thumbnail'>".__('Thumbnail',$mf_domain)."</label>".
+								"<p>".__('This post type have Thumbnail',$mf_domain)."</p>".
+								"<input type='checkbox' name='page_attributes'><label class='label_checkbox' for='thumbnail'>".__('Page Attributes',$mf_domain)."</label>".
+								"<p>".__('Page Attributes',$mf_domain)."</p>".
+							"</div>".
+						"</div>".
+					"</div>".	
+				"</form>".
+			"</div>";
 	}
 }
 ?>
