19ef0c2639c6a213252a7a5f0088ec751d3f781b
hunk <ing.edgar@gmail.com>
2011-04-26 23:21:22 -0500

    fix for Notice: has_cap

--- RCCWP_Menu.php
+++ RCCWP_Menu.php
@@ -233,7 +233,7 @@ class RCCWP_Menu
 
 		// Add top menu
 
-		add_menu_page(__('Magic Fields > Manage',$mf_domain), __('Magic Fields',$mf_domain), 10, 'MagicFieldsMenu', $panelsAndModulesFunctions->panelsMenuFunction, plugins_url(MF_PLUGIN_DIR.'/images/wand-hat.png'));
+		add_menu_page(__('Magic Fields > Manage',$mf_domain), __('Magic Fields',$mf_domain), 'edit_pages', 'MagicFieldsMenu', $panelsAndModulesFunctions->panelsMenuFunction, plugins_url(MF_PLUGIN_DIR.'/images/wand-hat.png'));
 		// Add Magic Fields submenus
 		add_submenu_page('MagicFieldsMenu', __('Write Panels',$mf_domain), __('Write Panels',$mf_domain), 'edit_pages','MagicFieldsMenu', $panelsAndModulesFunctions->panelsMenuFunction);		
 		
